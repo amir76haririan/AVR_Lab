397725a638d41c65a4b4ca14d270172ad5b373ea
amir76haririan <40703320+amir76haririan@users.noreply.github.com>
2020-12-14 12:24:26 +0330

    Update questionn_1.c

--- Task_1/questionn_1.c
+++ Task_1/questionn_1.c
@@ -1,1 +1,26 @@
-heloo
+#include <mega16.h>
+
+static unsigned int count;
+
+interrupt [TIM0_OVF] void timer0_ovf_isr(void){
+	TCNT0 = 56;
+	count ++;
+
+	if(count == 3787){
+		PORTA.0 = (PORTA.0 ^ 1);
+		count = 0;
+	}
+}
+
+void main(void){
+	DDRA = 0x01;
+  TCCR0 = 0x02;
+	TCNT0 = 56;
+  TIMSK = 0x01;
+
+	#asm("sei");
+
+	while(1){
+
+	}
+}
